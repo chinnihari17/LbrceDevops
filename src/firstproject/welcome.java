9d07c3ece8aacc7fe70e25455da0559010eebb9f
AdmiN <AdmiN@ITWS0056>
2024-08-20 10:24:30 +0530

    commit the data

--- src/firstproject/welcome.java
+++ src/firstproject/welcome.java
@@ -3,7 +3,8 @@ package firstproject;
 public class welcome {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub\
+		System.out.println("welcome to Devops");
 
 	}
 
